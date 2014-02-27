6a206226c6ccda838a5613c7d3d91612b2c8ccbb
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-02-27 20:08:41 +0100

    removed logo

--- search-everything/views/global_notice.php
+++ search-everything/views/global_notice.php
@@ -2,8 +2,5 @@
 	<div class="se-dismiss"><a href="<?php echo $close_url; ?>">Dismiss</a></div>
 	<h3><?php echo $notice['title']; ?></h3>
 	<p class="se-about-description"><?php echo $notice['message']; ?></p>
-	<div class="se-logo">
-		<a href="<?php echo SE_ZEMANTA_LOGO_URL; ?>" target="_blank"><img src="<?php echo (SE_PLUGIN_URL . '/static/img/zem-logo.png'); ?>" /></a>
-	</div>
 </div>
 
